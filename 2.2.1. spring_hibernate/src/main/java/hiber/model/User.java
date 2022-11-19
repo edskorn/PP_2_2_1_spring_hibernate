0824e4f50961c12f8d8d80980de1662e7eda6545
edskorn <ed.skorn@gmail.com>
2022-11-19 14:24:02 +0700

    added Car linked with User by OneToOne

--- 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
@@ -10,6 +10,10 @@ public class User {
    @GeneratedValue(strategy = GenerationType.IDENTITY)
    private Long id;
 
+   @OneToOne (cascade = CascadeType.ALL)
+   @JoinColumn(name="car_id")
+   private Car car;
+
    @Column(name = "name")
    private String firstName;
 
@@ -25,6 +29,11 @@ public class User {
       this.firstName = firstName;
       this.lastName = lastName;
       this.email = email;
+   }
+
+   public User(String firstName, String lastName, String email, Car car) {
+      this(firstName, lastName, email);
+      this.car = car;
    }
 
    public Long getId() {
@@ -57,5 +66,23 @@ public class User {
 
    public void setEmail(String email) {
       this.email = email;
+   }
+
+   public Car getCar() {
+      return car;
+   }
+
+   public void setCar(Car car) {
+      this.car = car;
+   }
+
+   @Override
+   public String toString() {
+      return "User{" +
+              "car=" + car +
+              ", firstName='" + firstName + '\'' +
+              ", lastName='" + lastName + '\'' +
+              ", email='" + email + '\'' +
+              '}';
    }
 }
